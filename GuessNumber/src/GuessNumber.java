381c997a8ea49f7a4675a3bb20d3d25c4e49d7e7
Srobkova <13svetka13@inbox.ru>
2016-11-01 12:10:16 +0300

    Commiting all GuessNumber files from my ocal machine
    I created GuessNumber project on my local computer and then install
    GitHub. Now trying to commit my project

---
+++ GuessNumber/src/GuessNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class GuessNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int number = 0 + (int)(Math.random() * ((10 - 0) + 1)); //разобраться как работает эта строчка
+		System.out.println(number); //for testing
+		
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input a number from 0 to 10: ");
+		int guess = in.nextInt();
+		
+		
+		if (guess == number) {
+			System.out.print("You win!");
+		}
+		else {
+			System.out.print("You loose!");
+		}
+		
+		
+		
+	}
+
+}
